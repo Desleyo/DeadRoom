5927cf5eb05242d31b5aa901c04ca90310ef43b1
gerranR <60340232+gerranR@users.noreply.github.com>
2020-11-12 17:42:43 +0100

    OnMouseOver

---
+++ DeadRoomGame/Assets/Gerran/Scripts/OnMouseOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseOver : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        GetComponent<Outline>().OutlineWidth = 10;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Outline>().OutlineWidth = 0;
+    }
+}
