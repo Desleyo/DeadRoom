0a58c10720da5429221add99c24430ecc3277e8f
Desleyo <Desleyoord@gmail.com>
2020-11-13 13:18:56 +0100

    key/lock test

---
+++ DeadRoomGame/Assets/Desley/Scripts/Key.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Key : MonoBehaviour
+{
+    public GameObject keyHole;
+    public float distance;
+    public bool unlocked;
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(gameObject.transform.position, keyHole.transform.position);
+        if(distance <= .3f && !unlocked)
+        {
+            //play sound
+            unlocked = true;
+            keyHole.GetComponentInParent<CircularDrive>().maxAngle = 90f;        
+        }
+    }
+}
