98b5e9141f55049be1aad7f9884f4287688fbb11
Desleyo <Desleyoord@gmail.com>
2020-11-19 16:02:41 +0100

    Update Socket.cs

--- DeadRoomGame/Assets/Desley/Scripts/Socket.cs
+++ DeadRoomGame/Assets/Desley/Scripts/Socket.cs
@@ -10,14 +10,25 @@ public class Socket : MonoBehaviour
     public GameObject item = null;
     public SteamVR_Action_Boolean input;
 
+    public bool once;
+
     // Update is called once per frame
     void Update()
     {
         if (item == null)
             return;
-
+        
+        if (!once)
+        {
             item.transform.position = transform.position;
             item.transform.rotation = Quaternion.identity;
+            once = true;
+        }
+
+        if(item.GetComponent<HandCollision>().collisionWithHand == true)
+        {
+
+        }
     }
 
     public void OnCollisionEnter(Collision collision)
