d9fdb8c7539184a6900ff5561b00abf53ec8e281
Desleyo <Desleyoord@gmail.com>
2020-11-19 11:35:32 +0100

    inventory test 1

---
+++ DeadRoomGame/Assets/Desley/Scripts/Socket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+public class Socket : MonoBehaviour
+{
+    public GameObject item = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(item != null)
+        {
+            item.transform.position = transform.position;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Interactable>())
+        {
+            item = collision.gameObject;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
