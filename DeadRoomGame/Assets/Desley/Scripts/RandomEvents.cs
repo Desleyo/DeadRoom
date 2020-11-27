2d4aa29866211e7d15539c8436569a1f0a45a23b
Desleyo <Desleyoord@gmail.com>
2020-11-27 17:25:26 +0100

    Random Events
    added sounds:
    door pound
    heartbeat
    scratching
    staring
    twig cracking
    walking on roof
    
    added events:
    head staring at you
    arm scratching the window

---
+++ DeadRoomGame/Assets/Desley/Scripts/RandomEvents.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEvents : MonoBehaviour
+{
+    public GameObject[] soundPositions;
+    public GameObject scratchPos, staringPos;
+    public int randomEvent, randomMax;
+    public float nextEventTimer, timerMin, timerMax;
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextEventTimer -= Time.deltaTime;
+
+        if(nextEventTimer <= 0)
+        {
+            randomEvent = Random.Range(0, randomMax);
+            nextEventTimer = Random.Range(timerMin, timerMax);
+            if(randomEvent >= 0 && randomEvent <= 2)
+            {
+                PlaySound(randomEvent);
+            }
+            else
+            {
+                PlayEvent(randomEvent);
+            }
+        }
+    }
+
+    public void PlaySound(int eventNumber)
+    {
+        soundPositions[eventNumber].GetComponent<SoundPlayer>().InstantiateSound();
+    }
+
+    public void PlayEvent(int eventNumber)
+    {
+        if(eventNumber == 3)
+        {
+            scratchPos.GetComponent<StairScratch>().Randomize();
+        }
+        else if(eventNumber == 4)
+        {
+            staringPos.GetComponent<StairScratch>().Randomize();
+        }
+        else if(eventNumber == 5)
+        {
+            //runBy
+        }
+    }
+}
---
+++ DeadRoomGame/Assets/Desley/Scripts/StairScratch.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairScratch : MonoBehaviour
+{
+    public GameObject[] children;
+    public GameObject activeChild, player;
+    public bool hasAnimationToPlay;
+    public int randomizer;
+    public float timer, actionTime;
+
+    public void Start()
+    {
+        timer = Mathf.Infinity;
+    }
+
+    public void Randomize()
+    {
+        randomizer = Random.Range(0, children.Length);
+        if (hasAnimationToPlay)
+        {
+            StartEvent();
+        }
+        else
+        {
+            player.GetComponent<AudioSource>().Play();
+            children[randomizer].SetActive(true);
+            activeChild = children[randomizer];
+            timer = actionTime;
+        }
+    }
+
+    public void StartEvent()
+    {
+        children[randomizer].SetActive(true);
+        activeChild = children[randomizer];
+        children[randomizer].GetComponent<Animator>().SetBool("Scratch", true);
+        timer = actionTime;
+    }
+
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0 && !hasAnimationToPlay)
+        {
+            activeChild.SetActive(false);
+            activeChild = null;
+            timer = Mathf.Infinity;
+        }
+        else if(timer <= 0)
+        {
+            activeChild.SetActive(false);
+            activeChild.GetComponent<Animator>().SetBool("Scratch", false);
+            activeChild = null;
+            timer = Mathf.Infinity;
+        }
+    }
+}
