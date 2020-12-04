301f9aab03d7f56c7806af5fd7a20bc009f018c8
gerranR <60340232+gerranR@users.noreply.github.com>
2020-12-04 14:26:24 +0100

    color Pattern

---
+++ DeadRoomGame/Assets/Gerran/Scripts/Chest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Chest : MonoBehaviour
+{
+    public SteamVR_Action_Boolean input;
+    public bool currentInput;
+    public float distance;
+    public GameObject controler, chest, colorPattern;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentInput = input.state;
+        distance = Vector3.Distance(controler.transform.position, chest.transform.position);
+        if (currentInput && distance <= 1)
+        {
+            colorPattern.SetActive(true);
+        }
+    }
+}
