a85878b0bd9b9db51339aeb02828e80df86b22db
gerranR <60340232+gerranR@users.noreply.github.com>
2020-12-04 12:52:07 +0100

    implemnted + flameRotation

---
+++ DeadRoomGame/Assets/Gerran/Scripts/StraightFlame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightFlame : MonoBehaviour
+{
+    public GameObject flame, glow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flame.transform.rotation = Quaternion.identity;
+        glow.transform.rotation = Quaternion.identity;
+    }
+}
