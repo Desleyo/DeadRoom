900fe5b2bc82ee5c5b546afb51648920b0e59b22
gerranR <60340232+gerranR@users.noreply.github.com>
2020-12-06 14:52:59 +0100

    chst implemented

--- DeadRoomGame/Assets/Gerran/Scripts/ColorPattern.cs
+++ DeadRoomGame/Assets/Gerran/Scripts/ColorPattern.cs
@@ -13,7 +13,7 @@ public class ColorPattern : MonoBehaviour
     public float time, baseTime;
 
     public SteamVR_Action_Boolean input;
-    public GameObject controller, closestColor, collerPattern;
+    public GameObject controller, closestColor, collerPattern, chest;
     public float distance, closestDistance = Mathf.Infinity;
     public bool currentInput;
 
@@ -115,6 +115,7 @@ public class ColorPattern : MonoBehaviour
                     colorMat[3].DisableKeyword("_EMISSION");
                     colorMat[4].DisableKeyword("_EMISSION");
                     collerPattern.SetActive(false);
+                    chest.GetComponent<CircularDrive>().minAngle = -50;
                 }
 
             }
