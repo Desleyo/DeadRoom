53dcf9aa50eceaec925ed94c5ce07d8a211bdb68
Desleyo <Desleyoord@gmail.com>
2020-12-09 20:24:49 +0100

    update main scene for test

--- DeadRoomGame/Assets/Gerran/Scripts/ColorPattern.cs
+++ DeadRoomGame/Assets/Gerran/Scripts/ColorPattern.cs
@@ -43,7 +43,7 @@ public class ColorPattern : MonoBehaviour
             canClick = true;
         }
 
-        if (canClick == true && !chestOpen)
+        if (canClick == true && !done)
         {
             if(currentInput && closestDistance <= 1)
             {
