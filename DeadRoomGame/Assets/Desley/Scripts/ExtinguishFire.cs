3a33c61374aad3c860dc9f1f25283b30166440dd
Desleyo <Desleyoord@gmail.com>
2020-12-21 14:40:39 +0100

    fire extinguisher

---
+++ DeadRoomGame/Assets/Desley/Scripts/ExtinguishFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ExtinguishFire : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public SteamVR_Action_Boolean Trigger;
+    public GameObject[] hands;
+    public GameObject fire;
+    public bool Grab;
+    public float distance = Mathf.Infinity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Grab = Trigger.state;
+        distance = Mathf.Infinity;
+
+        foreach (GameObject hand in hands)
+        {
+            distance = Vector3.Distance(transform.position, hand.transform.position);
+            if (distance <= .3 && Grab)
+            {
+                //play particle
+                fire.SetActive(false);
+                audioSource.Play();
+            }
+        }
+    }
+}
---
+++ DeadRoomGame/Assets/Desley/Scripts/FireExtinguisher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtinguisher : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject extinguisher;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, firePos.position);
+        if(distance <= 1)
+        {
+            extinguisher.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
