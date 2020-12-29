6c0f7224561c31b4641f584d7cdb3ee54fb59c55
Desleyo <Desleyoord@gmail.com>
2020-12-29 13:40:18 +0100

    menu, area and fadedFromBlack test

--- DeadRoomGame/Assets/Desley/Scripts/PlayerController.cs
+++ DeadRoomGame/Assets/Desley/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
@@ -10,18 +11,33 @@ public class PlayerController : MonoBehaviour
     public SteamVR_Action_Vector2 input;
     public int room;
     public float speed = 1;
+    public bool fadedFromBlack;
+    public Image image;
+    public Color alpha;
 
     public Transform checkPoint;
 
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+        alpha.a = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(input.axis.magnitude > .1f)
+        if (!fadedFromBlack)
+        {
+            alpha = image.GetComponent<Image>().color;
+            alpha.a -= .005f;
+            image.GetComponent<Image>().color = alpha;
+            if (alpha.a <= 0)
+            {
+                fadedFromBlack = true;
+            }
+        }
+
+        if (input.axis.magnitude > .1f)
         {
             Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
             controller.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
