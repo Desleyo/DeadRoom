49761a14948aee224e74afa71ebfa6378d84e233
Desleyo <Desleyoord@gmail.com>
2021-01-08 16:46:39 +0100

    buttons

---
+++ DeadRoomGame/Assets/Desley/Scripts/KeypadInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadInput : MonoBehaviour
+{
+    bool pressed;
+    public float pressDistance, originalPosY;
+
+    private void Start()
+    {
+        originalPosY = transform.position.y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Hand" && !pressed)
+        {
+            pressed = true;
+            transform.position = new Vector3(transform.position.x, transform.position.y - pressDistance, transform.position.z);
+            //add char to keypad screen
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            transform.position = new Vector3(transform.position.x, originalPosY, transform.position.z);
+            pressed = false;
+        }
+    }
+}
