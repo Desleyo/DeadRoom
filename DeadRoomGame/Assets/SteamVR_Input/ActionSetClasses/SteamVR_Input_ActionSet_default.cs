646ee72ac0b93ef742a1768f44caa6b20b7bd612
Desleyo <Desleyoord@gmail.com>
2021-01-12 17:56:59 +0100

    options button added

--- DeadRoomGame/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ DeadRoomGame/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -121,6 +121,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Options
+        {
+            get
+            {
+                return SteamVR_Actions.default_Options;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
