4a3deaa4d070a28a3cba1e596be03ba86653088a
Desleyo <Desleyoord@gmail.com>
2021-01-15 15:19:33 +0100

    Update OnMouseOver.cs

--- DeadRoomGame/Assets/Gerran/Scripts/OnMouseOver.cs
+++ DeadRoomGame/Assets/Gerran/Scripts/OnMouseOver.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class OnMouseOver : MonoBehaviour
 {
     public GameObject[] hands;
-
+    bool leftOutline;
     private void Update()
     {
         if (hands == null)
@@ -15,37 +15,26 @@ public class OnMouseOver : MonoBehaviour
         if (hands == null)
             return;
 
-        /*foreach(GameObject hand in hands)
-        {
-            float distance = Vector3.Distance(transform.position, hand.transform.position);
-            if(distance <= .2)
-            {
-                GetComponent<Outline>().OutlineWidth = 10;
-            }
-            else
-            {
-                GetComponent<Outline>().OutlineWidth = 0;
-            }
-        }*/
-
-        float distanceLeft = Vector3.Distance(transform.position, hands[1].transform.position);
-        if(distanceLeft <= .2)
-        {
-            GetComponent<Outline>().OutlineWidth = 10;
-        }
-        else
-        {
-            GetComponent<Outline>().OutlineWidth = 0;
-        }
-
         float distanceRight = Vector3.Distance(transform.position, hands[0].transform.position);
         if (distanceRight <= .2)
         {
             GetComponent<Outline>().OutlineWidth = 10;
         }
+        else if (!leftOutline)
+        {
+            GetComponent<Outline>().OutlineWidth = 0;
+        }
+
+        float distanceLeft = Vector3.Distance(transform.position, hands[1].transform.position);
+        if (distanceLeft <= .2)
+        {
+            GetComponent<Outline>().OutlineWidth = 10;
+            leftOutline = true;
+        }
         else
         {
             GetComponent<Outline>().OutlineWidth = 0;
+            leftOutline = false;
         }
     }
 }
